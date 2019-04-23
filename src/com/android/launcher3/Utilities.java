AospExtended/platform_packages_apps_Launcher3

Set a toggle to show Search bar on first screen
Instead of hardcoding it on the FeatureFlags, let's make it
toggleable as any other option would do

Change-Id: I59893308efa357d25bdd3d53e31904e90c4d84ca

src/com/android/launcher3/Utilities.java
@@ -68,6 +68,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.android.internal.util.aospextended.AEXUtils;
+
 /**
  * Various utilities shared amongst the Launcher's classes.
  */
@@ -672,6 +674,14 @@ public final class Utilities {
     static boolean hasFeedIntegration(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(Homescreen.KEY_FEED_INTEGRATION, true);
+    }
+
+    public static boolean showQSB(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        if (!AEXUtils.isPackageInstalled(context, LauncherTab.SEARCH_PACKAGE)) {
+            return false;
+        }
+        return prefs.getBoolean(Homescreen.KEY_SHOW_SEARCHBAR, true);
     }
 
     public static void restart(final Context context) {

Viperusgit/Launcher3

src/com/android/launcher3/Utilities.java
@@ -69,6 +69,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.android.internal.util.aquarios.AquaUtils;
+
 /**
  * Various utilities shared amongst the Launcher's classes.
  */
@@ -704,6 +706,14 @@ public final class Utilities {
     static boolean hasFeedIntegration(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getBoolean(Homescreen.KEY_FEED_INTEGRATION, true);
+    }
+
+    public static boolean showQSB(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        if (!AquaUtils.isPackageInstalled(context, LauncherTab.SEARCH_PACKAGE)) {
+            return false;
+        }
+        return prefs.getBoolean(Homescreen.KEY_SHOW_SEARCHBAR, true);
     }
 
     public static void restart(final Context context) {