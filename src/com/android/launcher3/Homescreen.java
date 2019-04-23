ezio84/abc_packages_apps_Launcher3

Utilize the Utils in the fwb
Change-Id: I8bcc34d22505f1db421b6d9797b3af3b905bda8e

src/com/android/launcher3/Homescreen.java
@@ -22,8 +22,8 @@ import static com.android.launcher3.states.RotationHelper.getAllowRotationDefaul
 import android.app.ActionBar;
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -31,6 +31,8 @@ import android.preference.PreferenceFragment;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.SwitchPreference;
 import android.view.MenuItem;
+
+import com.android.internal.util.du.Utils;
 
 public class Homescreen extends SettingsActivity implements PreferenceFragment.OnPreferenceStartFragmentCallback {
 
@@ -60,11 +62,15 @@ public class Homescreen extends SettingsActivity implements PreferenceFragment.O
 
         ActionBar actionBar;
 
+        private Context mContext;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             getPreferenceManager().setSharedPreferencesName(LauncherFiles.SHARED_PREFERENCES_KEY);
             addPreferencesFromResource(R.xml.home_screen_preferences);
+
+            mContext = getActivity();
 
             actionBar=getActivity().getActionBar();
             assert actionBar != null;
@@ -72,7 +78,8 @@ public class Homescreen extends SettingsActivity implements PreferenceFragment.O
 
             SwitchPreference feedIntegration = (SwitchPreference)
                     findPreference(KEY_FEED_INTEGRATION);
-            if (!hasPackageInstalled(LauncherTab.SEARCH_PACKAGE)) {
+
+            if (!Utils.isPackageInstalled(mContext, LauncherTab.SEARCH_PACKAGE)) {
                 getPreferenceScreen().removePreference(feedIntegration);
             }
 
@@ -148,16 +155,6 @@ public class Homescreen extends SettingsActivity implements PreferenceFragment.O
         @Override
         public boolean onPreferenceClick(Preference preference) {
             return false;
-        }
-
-        private boolean hasPackageInstalled(String pkgName) {
-            try {
-                ApplicationInfo ai = getContext().getPackageManager()
-                        .getApplicationInfo(pkgName, 0);
-                return ai.enabled;
-            } catch (PackageManager.NameNotFoundException e) {
-                return false;
-            }
         }
     }
 

Viperusgit/Launcher3

src/com/android/launcher3/Homescreen.java
@@ -22,8 +22,8 @@ import static com.android.launcher3.states.RotationHelper.getAllowRotationDefaul
 import android.app.ActionBar;
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -31,6 +31,8 @@ import android.preference.PreferenceFragment;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.SwitchPreference;
 import android.view.MenuItem;
+
+import com.android.internal.util.aquarios.AquaUtils;
 
 public class Homescreen extends SettingsActivity implements PreferenceFragment.OnPreferenceStartFragmentCallback {
 
@@ -60,11 +62,15 @@ public class Homescreen extends SettingsActivity implements PreferenceFragment.O
 
         ActionBar actionBar;
 
+        private Context mContext;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             getPreferenceManager().setSharedPreferencesName(LauncherFiles.SHARED_PREFERENCES_KEY);
             addPreferencesFromResource(R.xml.home_screen_preferences);
+
+            mContext = getActivity();
 
             actionBar=getActivity().getActionBar();
             assert actionBar != null;
@@ -72,7 +78,8 @@ public class Homescreen extends SettingsActivity implements PreferenceFragment.O
 
             SwitchPreference feedIntegration = (SwitchPreference)
                     findPreference(KEY_FEED_INTEGRATION);
-            if (!hasPackageInstalled(LauncherTab.SEARCH_PACKAGE)) {
+
+            if (!AquaUtils.isPackageInstalled(mContext, LauncherTab.SEARCH_PACKAGE)) {
                 getPreferenceScreen().removePreference(feedIntegration);
             }
 
@@ -148,16 +155,6 @@ public class Homescreen extends SettingsActivity implements PreferenceFragment.O
         @Override
         public boolean onPreferenceClick(Preference preference) {
             return false;
-        }
-
-        private boolean hasPackageInstalled(String pkgName) {
-            try {
-                ApplicationInfo ai = getContext().getPackageManager()
-                        .getApplicationInfo(pkgName, 0);
-                return ai.enabled;
-            } catch (PackageManager.NameNotFoundException e) {
-                return false;
-            }
         }
     }
 